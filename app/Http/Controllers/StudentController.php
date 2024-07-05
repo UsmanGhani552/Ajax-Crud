8f4666c448adcc7ee3325eb82ef08bad269a3164
UsmanGhani552 <usman.centosquare@gmail.com>
2024-07-05 18:44:16 +0500

    first commit

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        return view('student.index');
+    }
+
+    public function fetchstudent()
+    {
+        $students = Student::all();
+        return response()->json([
+            'students' => $students,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:191',
+            'course' => 'required|max:191',
+            'email' => 'required|email|max:191',
+            'phone' => 'required',
+            'image' => 'required|image',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->messages()
+            ]);
+        } else {
+            $student = new Student;
+            $student->name = $request->input('name');
+            $student->course = $request->input('course');
+            $student->email = $request->input('email');
+            $student->phone = $request->input('phone');
+
+            if($request->hasFile('image')){
+                $image = $request->file('image');
+                $filename = time() .'.'. $image->getClientOriginalName();
+                $image->move('images', $filename);
+                $student->image = $filename;
+            }
+
+            $student->save();
+            return response()->json([
+                'status' => 200,
+                'message' => 'Student Added Successfully.'
+            ]);
+        }
+    }
+
+    public function edit($id)
+    {
+        $student = Student::find($id);
+        if ($student) {
+            return response()->json([
+                'status' => 200,
+                'student' => $student,
+            ]);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'No Student Found.'
+            ]);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:191',
+            'course' => 'required|max:191',
+            'email' => 'required|email|max:191',
+            'phone' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->messages()
+            ]);
+        } else {
+            $student = Student::find($id);
+            if ($student) {
+                $student->name = $request->input('name');
+                $student->course = $request->input('course');
+                $student->email = $request->input('email');
+                $student->phone = $request->input('phone');
+                if($request->hasFile('image')){
+                    $image = $request->file('image');
+                    $filename = time() .'.'. $image->getClientOriginalName();
+                    $image->move('images', $filename);
+                    $student->image = $filename;
+                }
+                $student->update();
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Student Updated Successfully.'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'No Student Found.'
+                ]);
+            }
+        }
+    }
+
+    public function destroy($id)
+    {
+        $student = Student::find($id);
+        if ($student) {
+            $student->delete();
+            return response()->json([
+                'status' => 200,
+                'message' => 'Student Deleted Successfully.'
+            ]);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'No Student Found.'
+            ]);
+        }
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+    <div id="app">
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+
+    </div>
+
+    <!-- Scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
+    @yield('scripts')
+
+</body>
+</html>
